eec7695e52d16bd5ce887beb1402e811e43ac1d4
Indy Sam <indysam221b@gmail.com>
2013-08-21 15:23:07 +0530

    Modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var newResponse = new Buffer(fs.readFileSync("index.html"));
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+	response.send(newResponse.toString());
 });
 
 var port = process.env.PORT || 5000;
